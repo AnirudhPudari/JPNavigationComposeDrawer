31bdffa7e1b29d75cf2ae1035052f9157bd36606
Anirudhgoud <auroraanirudh@gmail.com>
2023-03-21 22:59:45 +0530

    minor refactors

--- app/src/main/java/com/example/jpsampledrawer/AppDrawer.kt
+++ app/src/main/java/com/example/jpsampledrawer/AppDrawer.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jpsampledrawer.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
--- app/src/main/java/com/example/jpsampledrawer/MainActivity.kt
+++ app/src/main/java/com/example/jpsampledrawer/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.jpsampledrawer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jpsampledrawer.ui.theme.SampleDrawerTheme
 
